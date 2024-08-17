92a6fd549eaf5cc8b386fe0c9a6a482e216a6cd9
knoxy_ <83923964+su1cidal@users.noreply.github.com>
2024-08-17 15:11:50 +0300

    Add main menu button to FirstLevelWindow

--- GMTK Game Jam 2024/Assets/Scripts/FirstLevel/FirstLevelWindow.cs
+++ GMTK Game Jam 2024/Assets/Scripts/FirstLevel/FirstLevelWindow.cs
@@ -1,14 +1,27 @@
-﻿using UnityEngine;
+﻿using Core;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace FirstLevel
 {
     public class FirstLevelWindow : MonoBehaviour
     {
+        [SerializeField] private Button _mainMenuButton;
         private FirstLevelController _levelController;
         
         public void Initialize(FirstLevelController levelController)
         {
             _levelController = levelController;
+        }
+
+        private void Awake()
+        {
+            _mainMenuButton.onClick.AddListener(OnMainMenuButtonClicked);
+        }
+        
+        private void OnMainMenuButtonClicked()
+        {
+            SceneLoader.Instance.LoadSceneWithLoadingScreen(Constants.Scenes.MainMenu);
         }
     }
 }

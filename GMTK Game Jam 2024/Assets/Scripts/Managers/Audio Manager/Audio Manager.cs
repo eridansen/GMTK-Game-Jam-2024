d22223db7b426cab1152c3c6e4aee1f8fe8c1b9d
knoxy_ <83923964+su1cidal@users.noreply.github.com>
2024-08-18 17:29:37 +0300

    Modify AudioManager

--- GMTK Game Jam 2024/Assets/Scripts/Managers/Audio Manager/Audio Manager.cs
+++ GMTK Game Jam 2024/Assets/Scripts/Managers/Audio Manager/Audio Manager.cs
@@ -1,40 +1,58 @@
 using Helpers;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : Singleton<AudioManager>
 {
-    [SerializeField] 
-    private AudioSource musicSource, sfxSource;
-    public Sound[] musicSounds, sfxSounds;
+    [SerializeField] private AudioSource _musicSource;
+    [SerializeField] private AudioSource _sfxSource;
+    [SerializeField] private Sound[] _musicSounds;
+    [SerializeField] private Sound[] _sfxSounds;
     
+    public void SetMusicVolume(float volume)
+    {
+        _musicSource.volume = Mathf.Clamp(volume, 0f, 1f);
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        _sfxSource.volume = Mathf.Clamp(volume, 0f, 1f);
+    }
+
+    public float GetMusicVolume()
+    {
+        return _musicSource.volume;
+    }
+
+    public float GetSfxVolume()
+    {
+        return _sfxSource.volume;
+    }
 
     public void PlayMusic(string name)
     {
-        Sound s = Array.Find(musicSounds, s => s.name == name);
+        Sound s = Array.Find(_musicSounds, s => s.name == name);
         if (s == null)
         {
             Debug.Log("Sound does not exist");
         }
         else
         {
-            musicSource.clip = s.clip;
-            musicSource.Play();
+            _musicSource.clip = s.clip;
+            _musicSource.Play();
         }
     }
 
     public void PlaySfx(string name)
     {
-        Sound s = Array.Find(sfxSounds, s => s.name == name);
+        Sound s = Array.Find(_sfxSounds, s => s.name == name);
         if (s == null)
         {
             Debug.Log("Sound does not exist");
         }
         else
         {
-            sfxSource.PlayOneShot(s.clip);
+            _sfxSource.PlayOneShot(s.clip);
         }
     }
 }

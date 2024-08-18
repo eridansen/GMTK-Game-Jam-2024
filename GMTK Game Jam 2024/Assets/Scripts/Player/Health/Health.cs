d07b58db1621ac75d6cd68e4145c438d9e2c82aa
Boryslavc <152865471+Boryslavc@users.noreply.github.com>
2024-08-18 10:57:31 +0200

    Modified HealthBar

--- GMTK Game Jam 2024/Assets/Scripts/Player/Health/Health.cs
+++ GMTK Game Jam 2024/Assets/Scripts/Player/Health/Health.cs
@@ -2,41 +2,32 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Health : MonoBehaviour
+public class Health: MonoBehaviour
 {
-    [SerializeField] private float _maxHealth;
     [SerializeField] private Image _healthBar;
-
+    [SerializeField] private float _maxHealth;
+    [SerializeField] [Range(0.01f,0.1f)] private float _animationSpeed;
+    
     private float currentHealth;
+
+
     private void Awake()
     {
         currentHealth = _maxHealth;
     }
 
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.B))
-        {
-            if(currentHealth <= 0) 
-                _healthBar.enabled = false;
-            StartCoroutine(TakeDamage(10));
-        }
-        if(Input.GetKeyDown(KeyCode.N))
-        {
-            StartCoroutine(RestoreHealth(10));
-        }
-    }
-
     public IEnumerator TakeDamage(float damage)
     {
         currentHealth -= damage;
+        if (currentHealth <= 0) 
+            currentHealth = 0;
 
         float initValue = _healthBar.fillAmount;
         float percent = currentHealth / _maxHealth;
 
         float iterator = 0;
 
-        while(iterator < 1) 
+        while (iterator < 1)
         {
             _healthBar.fillAmount = Mathf.Lerp(initValue, percent, iterator);
             iterator += 0.05f;
@@ -44,11 +35,11 @@ public class Health : MonoBehaviour
         }
     }
 
-   
-
     public IEnumerator RestoreHealth(float health)
     {
         currentHealth += health;
+        if(currentHealth > _maxHealth)
+            currentHealth = _maxHealth;
 
         float initValue = _healthBar.fillAmount;
         float percent = currentHealth / _maxHealth;

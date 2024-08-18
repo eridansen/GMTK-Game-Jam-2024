07c321a80798eeea5c3af6cc544509c256335fde
Jkingyy <jkingy2000@gmail.com>
2024-08-18 18:51:49 +0100

    Moved ground check to players back foor
    makes it more reliable when standing near edge of platform

--- GMTK Game Jam 2024/Assets/Scripts/Player/Player Movement.cs
+++ GMTK Game Jam 2024/Assets/Scripts/Player/Player Movement.cs
@@ -147,7 +147,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask _groundMask; // The layer mask for ground objects
     [SerializeField] private Transform _groundCheck; // The transform representing the position to check for ground
     [SerializeField] private float _groundCheckRadius = 0.4f; // The radius for ground check
-    [SerializeField] private bool isGrounded; // Flag indicating if the player is grounded
+    private bool isGrounded; // Flag indicating if the player is grounded
 
     // Check if the player is grounded
     private void Grounded()
